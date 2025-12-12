48610b150b305d3f1318101a845fa712f36b9c99
Nahla-Nabil <nahla.nabil.52@gmail.com>
2025-12-12 03:45:36 +0300

    Create 1_About.py

---
+++ Real /pages/1_About.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("ℹ️ About the Project")
+st.write("""
+This AI system detects **fake job postings** using a fine-tuned RoBERTa language model.
+
+### Features:
+- Real/Fake Classification  
+- Bilingual Interface (EN / AR)  
+- Dark/Light Theme  
+- Explainability Page  
+- Clean UI for deployment  
+
+Made by: **Nahla Nabil Skaik**  
+University: Arab Open University – Bahrain  
+""")
